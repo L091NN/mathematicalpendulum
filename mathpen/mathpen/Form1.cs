2820931f6eea860ae571bec23f7258a2dfc8d613
Михаил Лугин <329898@mail.ru>
2019-11-11 13:52:14 +0300

    Top dezigner

---
+++ mathpen/mathpen/Form1.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ZedGraph;
+
+namespace mathpen
+{
+    public partial class Form1 : Form
+    {
+        public Color[] color = { Color.Red, Color.Yellow, Color.Green, Color.Blue, Color.Black};
+        GraphPane pane1;
+        GraphPane pane2;
+        int steps;
+        public Method method = new Method();
+        public Form1()
+        {
+            InitializeComponent();
+            pane1 = zedGraphControl1.GraphPane;
+            pane2 = zedGraphControl2.GraphPane;
+            this.ClientSize = new System.Drawing.Size(1600, 900);
+            this.tabControl1.Size = new System.Drawing.Size(this.ClientSize.Width, this.ClientSize.Height);
+            this.panel1.Hide();
+            this.progressBar1.Hide();
+            this.checkBox1.Text = "Очищать график";
+            this.checkBox2.Text = "Очищать график";
+            this.comboBox1.Text = this.comboBox1.Items[0].ToString();
+            this.comboBox2.Text = this.comboBox2.Items[0].ToString();
+            this.domainUpDown1.SelectedIndex = 1;
+            this.domainUpDown2.SelectedIndex = 2;
+            this.textBoxU0.Text = (Math.PI / 10.0).ToString();
+            this.textBoxdU0.Text = (0.0).ToString();
+            this.textBoxX0.Text = (0.0).ToString();
+            this.textBoxH.Text = (0.001).ToString();
+            this.textBoxL.Text = (0.1).ToString();
+            this.textBoxG.Text = (9.8).ToString();
+            this.textBoxE.Text = (0.0001).ToString();
+            this.textBoxMaxH.Text = (1000).ToString();
+            this.textBoxGrX.Text = (1.0).ToString();
+        }
+        protected override CreateParams CreateParams
+        {
+            get
+            {
+                const int WS_SIZEBOX = 0x40000;
+
+                var cp = base.CreateParams;
+                cp.Style |= WS_SIZEBOX;
+
+                return cp;
+            }
+        }
+
+        public PointPair WhatNeed1(double h)
+        {   
+            if(this.domainUpDown1.SelectedIndex == 1)
+                return new PointPair(method.x, method.v1);
+            if (this.domainUpDown1.SelectedIndex == 2)
+                return new PointPair(method.x, method.v2);
+            if (this.domainUpDown1.SelectedIndex == 3)
+                return new PointPair(method.x, method.v3);
+            if (this.domainUpDown1.SelectedIndex == 4)
+                return new PointPair(method.v1, method.v2);
+            if (this.domainUpDown1.SelectedIndex == 5)
+                return new PointPair(h, method.diffV);
+            return new PointPair(0, 0);
+        }
+        public PointPair WhatNeed2(double h)
+        {
+            if (this.domainUpDown2.SelectedIndex == 1)
+                return new PointPair(method.x, method.v1);
+            if (this.domainUpDown2.SelectedIndex == 2)
+                return new PointPair(method.x, method.v2);
+            if (this.domainUpDown2.SelectedIndex == 3)
+                return new PointPair(method.x, method.v3);
+            if (this.domainUpDown2.SelectedIndex == 4)
+                return new PointPair(method.v1, method.v2);
+            if (this.domainUpDown2.SelectedIndex == 5)
+                return new PointPair(h, method.diffV);
+            return new PointPair(0, 0);
+        }
+
+        public bool RUN()
+        {
+            //чек нужных графиков
+            //обнуление инфы
+            //запуск чм
+            //прогресс прогрессбара
+            //запись элементов в массивы
+            //чек цвета
+            //рисовка
+            if (checkBox1.Checked)
+            pane1.CurveList.Clear();
+            if (checkBox2.Checked)
+                pane2.CurveList.Clear();
+            PointPairList list1 = new PointPairList();
+            PointPairList list2 = new PointPairList();
+            method.v1 = double.Parse(textBoxU0.Text);
+            method.v2 = double.Parse(this.textBoxdU0.Text);
+            method.x = double.Parse(this.textBoxX0.Text);
+            method.h = double.Parse(this.textBoxH.Text);
+            method.functions.L = double.Parse(this.textBoxL.Text);
+            method.functions.g = double.Parse(this.textBoxG.Text);
+            method.eps = double.Parse(this.textBoxE.Text);
+            steps = int.Parse(this.textBoxMaxH.Text);
+            method.xMax = double.Parse(this.textBoxGrX.Text);
+            progressBar1.Value++;
+
+            for (int curH = 0; curH < steps; curH++)
+            {
+                method.OptimizationStep();
+                method.Step();
+                list1.Add(WhatNeed1(curH));
+                list2.Add(WhatNeed2(curH));
+            }
+
+            LineItem li1 = pane1.AddCurve("", list1, color[comboBox1.SelectedIndex], SymbolType.None);
+            LineItem li2 = pane2.AddCurve("", list2, color[comboBox2.SelectedIndex], SymbolType.None);
+            zedGraphControl1.AxisChange();
+            zedGraphControl1.Invalidate();
+            zedGraphControl2.AxisChange();
+            zedGraphControl2.Invalidate();
+            return true;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //запуск прогресбара
+            //конец
+            this.panel1.Visible = true;
+            this.progressBar1.Value = 0;
+            this.progressBar1.Visible = true;
+
+            RUN();
+
+            this.progressBar1.Visible = false;
+            this.panel1.Visible = false;
+        }
+    }
+}
---
+++ mathpen/mathpen/method.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mathpen
+{
+    public class Method
+    {
+        public double x { set; get; }
+        public double xMax { set; get; }
+        public double v1 { set; get; }
+        public double v2 { set; get; }
+        public double v3 { set; get; }
+
+        public double h { set; get; }
+        public double hMax { set; get; }
+        public double hMin { set; get; }
+        public double eps { set; get; }
+
+        public double s { set; get; }
+        public double diffV { set; get; }
+        public double maxDiffV { set; get; }
+        public bool maxDiffChange{ set; get; }
+        public int doubles { set; get; }
+        public int divides { set; get; }
+
+        public double[] k = new double[4];
+        public double[] m = new double[4];
+
+        public double v1_2 { set; get; }
+        public Functions functions = new Functions();
+
+        private double TryStep(double _x, double _u1, double _u2, double _h, int _steps, bool save = false)
+        {
+            double curX = _x;
+            double curU1 = _u1;
+            double curU2 = _u2;
+            while (_steps-- > 0)
+            {
+                k[0] = functions.f1(curX, curU1, curU2) * _h;
+                m[0] = functions.f2(curX, curU1, curU2) * _h;
+                k[1] = functions.f1(curX, curU1 + k[0] / 2.0, curU2 + m[0] / 2.0) * _h;
+                m[1] = functions.f2(curX, curU1 + k[0] / 2.0, curU2 + m[0] / 2.0) * _h;
+                k[2] = functions.f1(curX, curU1 + k[1] / 2.0, curU2 + m[1] / 2.0) * _h;
+                m[2] = functions.f2(curX, curU1 + k[1] / 2.0, curU2 + m[1] / 2.0) * _h;
+                k[3] = functions.f1(curX, curU1 + k[2], curU2 + m[2]) * _h;
+                m[3] = functions.f2(curX, curU1 + k[2], curU2 + m[2]) * _h;
+
+                curX += _h;
+                curU1 += 1.0 / 6.0 * (k[0] + 2.0 * k[1] + 2.0 * k[2] + k[3]);
+                curU2 += 1.0 / 6.0 * (m[0] + 2.0 * m[1] + 2.0 * m[2] + m[3]);
+            }
+            if (save)
+            {
+                x = curX;
+                v1 = curU1;
+                v2 = curU2;
+                v3 = functions.f1(curX, curU1, curU2);
+            }
+            return curU1;
+        }
+
+        public void OptimizationStep()
+        {
+            double u = TryStep(x, v1, v2, h, 1);
+            double _u = TryStep(x, v1, v2, h / 2.0, 1);
+            v1_2 = _u;
+            _u = TryStep(x, v1, v2, h / 2.0, 1);
+            diffV = Math.Abs(_u - u);
+            s = diffV / 8.0;
+
+            while (s > eps || s < eps / 16.0)
+            {
+                if (s > eps)
+                {
+                    h /= 2;
+                    u = TryStep(x, v1, v2, h, 1);
+                    _u = TryStep(x, v1, v2, h / 2.0, 1);
+                    v1_2 = _u;
+                    _u = TryStep(x, v1, v2, h / 2.0, 1);
+                    diffV = Math.Abs(_u - u);
+                    s = diffV / 8.0;
+                    divides++;
+                    continue;
+                }
+                if (s < eps / 16.0)
+                {
+                    h *= 2;
+                    u = TryStep(x, v1, v2, h, 1);
+                    _u = TryStep(x, v1, v2, h / 2.0, 1);
+                    v1_2 = _u;
+                    _u = TryStep(x, v1, v2, h / 2.0, 1);
+                    diffV = Math.Abs(_u - u);
+                    s = diffV / 8.0;
+                    doubles++;
+                }
+            }
+            if (diffV > maxDiffV)
+            {
+                maxDiffV = diffV;
+                maxDiffChange = true;
+            }
+            else
+            {
+                maxDiffChange = false;
+            };
+        }
+        public void Step()
+        {
+            TryStep(x, v1, v2, h, 1, true);
+        }
+    }
+}
